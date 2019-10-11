bb768ae3172cecdccfd49b7919b52bb237bc3aa4
Alexander Shilen <aps2163@columbia.edu>
2019-10-11 14:00:31 -0400

    use fancylogger for all the plugins and fix confignotfound msg indent

--- sykle/cli.py
+++ sykle/cli.py
@@ -87,9 +87,10 @@ def _load_config(args):
     try:
         return Config.from_file(config_name)
     except Config.ConfigFileNotFoundException:
-        message = """Config file %s does not exist!
-            You can create an empty config by running:
-            \tsyk init""" % config_name
+        message = """
+Config file %s does not exist!
+You can create an empty config by running:
+\tsyk init""" % config_name
         logger.critical(message)
     except Config.ConfigFileDecodeException as e:
         logger.critical('Config Decode Error: %s' % e)
@@ -107,10 +108,10 @@ def _get_docker_type(args):
 
 def use_run_file():
     logger.warn(
-        """========================UPGRADE===========================
-                         Legacy run.sh detected!
-          Will try to run commands through ./run.sh until removed
-        =========================================================="""
+        "========================UPGRADE==========================="
+        "                 Legacy run.sh detected!                  "
+        "  Will try to run commands through ./run.sh until removed "
+        "=========================================================="
     )
     time.sleep(1)
 
